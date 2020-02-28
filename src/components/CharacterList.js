a6fcf456c58f601a16b8b0acc966857a59ec679a
Idong Essien <idong.essien@comcast.net>
2020-02-28 18:11:47 -0500

    Created and added Search functionality

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard.js";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   const [char, setChar] = useState([]);
 
   useEffect(() => {
     axios
-    .get("https://rickandmortyapi.com/api/character/?page=1")
+    .get("https://rickandmortyapi.com/api/character/")
     .then(response => setChar(response.data.results))
     .catch(error => {
       console.log("The data was not returned", error);
@@ -17,6 +18,7 @@ export default function CharacterList() {
   return (
     <section className="character-list">
         <div>
+          <SearchForm />
           { char.map((chars, index) => (
             <CharacterCard
               name = { chars.name }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function SearchForm() {
+
+  const [searchWord, setSearchWord] = useState("");
+  const [searchRes, setSearchRes] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get("https://rickandmortyapi.com/api/character/")
+    .then(response => setSearchRes(response.data.results))
+    .catch(error => {
+      console.log("The data was not returned", error);
+});
+const results = searchRes.filter(query => 
+  query.toLowerCase().includes(searchWord.toLowerCase())
+);
+  setSearchRes(results);
+  }, [searchWord]);
+
+  const handleChange = e => {
+    setSearchWord(e.target.value);
+  };
  
   return (
     <section className="search-form">
-     // Add a search form here
+     <div>
+       <form>
+         <input id="name" type="text" name="textbox" placeholder="Search" value={ searchWord } onChange={ handleChange } />
+       </form>
+       {/* <div>
+         <ul>
+           { searchRes.map(query => (
+             <li key = { query }>[ query ]</li>
+           )) }
+         </ul>
+       </div> */}
+     </div>
     </section>
   );
 }
