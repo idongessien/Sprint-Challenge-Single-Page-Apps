12ee89c19f706c79998c7cc8bdf7920213bef9d4
Idong Essien <idong.essien@comcast.net>
2020-02-28 14:02:43 -0500

    commit :(

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Header from "./components/Header.js";
 
 
-export default function App() {
-  return (
-    <main>
+const App = () => {
+  const [page, setPage] = useState(1);
+  const [char, setChar] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get(`https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/?page=${page}`)
+    .then(response => console.log(response.data.results));
+}, [page]);
+
+    return (
+        <div>
+          <main>
       <Header />
     </main>
-  );
+            <button>Previous</button>
+            <button>Next</button>
+        </div>
+        );  
 }
+
+export default App;
