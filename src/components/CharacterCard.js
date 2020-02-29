a16c4d00c8ce4167a4a9d3868fe1cb74cb7589b5
Idong Essien <idong.essien@comcast.net>
2020-02-28 19:54:00 -0500

    styles updated for Characters component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -6,12 +6,18 @@ const IndivCards = styled.div`
   padding: 2%;
   filter: drop-shadow(5px 5px 5px gray);
   border-radius: 10px;
+  width: 20%;
+  margin: 2% auto;
+`
+const Images = styled.img`
+  border-radius: 10px;
+  width: 100%;
 `
 
 export default function CharacterCard(props) {
   return (
   <IndivCards>
-    <img src = { props.image } />
+    <Images src = { props.image } />
     <p>Name: { props.name }</p>
     <p>Status: { props.status }</p>
     <p>Gender: { props.gender }</p>
